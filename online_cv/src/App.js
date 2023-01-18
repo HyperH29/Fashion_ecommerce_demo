e2a50b8b942c55865b0f8e2cf56dc9ed1d763cc5
Hamza <hkbenjamin1994@gmail.com>
2023-01-18 18:01:20 +0000

    Installed Mul-ui and removed bonsai

--- online_cv/src/App.js
+++ online_cv/src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
   return (
     <div className="App">
+      <div className="Box">
       <h1>Hello World</h1>
+        <h6>My name is <span className="name">John Doe</span></h6>
+    <p>The is a test of all the stuff i have learnt from the course and before when i was training on my own. </p>
+      <button className="green">Green</button>
+        <button className="grey">Grey</button>
+
+      </div>
     </div>
   );
 }
 
 export default App;
+// Get ready to install Bonsai
