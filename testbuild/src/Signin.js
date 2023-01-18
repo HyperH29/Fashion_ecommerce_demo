b5c20fa725c0c7722061502ba938a8f98f35a14b
Hamza <hkbenjamin1994@gmail.com>
2023-01-18 12:46:56 +0000

    Starting up new project, deleted and adjusted files

--- testbuild/src/Signin.js
+++ testbuild/src/Signin.js
@@ -3,78 +3,42 @@ import Form from 'react-bootstrap/Form';
 import logo1 from './logo1.svg';
 import './form.css';
 import {useState} from "react";
+import {Navigate} from "react-router-dom";
+import Signin from './Nav1'
 
-//
-let input = document.getElementsByTagName('Form.Control');
-let button = document.querySelectorAll('Button');
-// button.forEach((btn) => {
-//     btn.addEventListener('click', () => {
-//         btn.classList.add('loading');
-//         setTimeout(() => {
-//             btn.classList.remove('loading');
-//         }, 3000);
-//     });
-//     }
-// ); Not working
-
+// If user information is correct then redirect to the home page
 let user = {
     name: "user",
-    email: "user.@gmail.com",
+    email: "user@gmail.com",
     password: "p1"
 }
 
-function CreateForm() {
-    return (
-        <div  className="signin-card bg-light">
-            <img width={150} className= "sign-logo" src={logo1} alt={logo1}/>
-            <Form>
-            <Form.Label>Sign In {user.name}</Form.Label>
-            <Form.Group className="mb-4" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" xs={4}/>
-            </Form.Group>
-            <Form.Group className="mb-4" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Save Password"/>
-            </Form.Group>
-            <Button variant="dark" type="submit" >
-                Sign In</Button>
-        {/*Once sign is press button will load and hold for a few seconds then enter the main product page */}
-        {/*        If the user enters the wrong details they will be asked to resubmit. */}
-        </Form>
-        </div>
-    );
-}
-
+// Won't need the above code for later
 // If user signs in successfully, redirect to the home page
 // else display error message
-//export default CreateForm;
 
 const SignIn = () => {
     const [view, setView] = useState('signin');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState('user@gmail.com');
+    const [password, setPassword] = useState('p1'); // This is a placeholder for the password
 
+    // If the value of user is this then redirect to the home page
+
+    // If the user is not found then display error message
     const handleSignIn = () => {
-        // setView()
-        // in setView() we will set the view to the home page
-        // For example: setView('home')
-        setView('home');
-        // How do we set the view to the home page? : setView('home') ? : then what?
+        if (email === user.email && password === user.password) {
+            setEmail('user@gmail.com');
+            setPassword('p1');
+            setView(<Navigate to={'/home'}/>);
+        }
         // Use router to set the view to the home page
 
     };
     if (view === "signin") {
         return (
+            <div>
+            <Signin />
             <div className="signin-card bg-light">
                 <img width={150} className= "sign-logo" src={logo1} alt={logo1}/>
                 <Form>
@@ -94,11 +58,12 @@ const SignIn = () => {
                     <Form.Group className="mb-4" controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Save Password"/>
                     </Form.Group>
-                    <Button variant="dark" type="submit" onClick={handleSignIn}>
+                    <Button variant="dark" type="submit" onClick={() => handleSignIn()}>
                         Sign In</Button>
                     {/*Once sign is press button will load and hold for a few seconds then enter the main product page */}
                     {/*        If the user enters the wrong details they will be asked to resubmit. */}
                 </Form>
+            </div>
             </div>
         )
     } else {
@@ -108,5 +73,6 @@ const SignIn = () => {
             </div>
         )
     }
+
 }
 export default SignIn;
