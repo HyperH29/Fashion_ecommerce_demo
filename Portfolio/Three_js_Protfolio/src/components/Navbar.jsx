697b25fe6e1b42a4ff3c18f7084b4fa502a2f327
Hamza <hkbenjamin1994@gmail.com>
2023-03-09 11:28:42 +0000

    Created a responsive navbar and added a logo and subtitles.

--- Portfolio/Three_js_Protfolio/src/components/Navbar.jsx
+++ Portfolio/Three_js_Protfolio/src/components/Navbar.jsx
@@ -6,9 +6,11 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
-      className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="flex items-center justify-between w-full max-w-7xl mx-auto">
         <Link
@@ -19,8 +21,66 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt={logo} className="w-9 h9 object-contain" />
+          <img
+            src={logo}
+            alt={logo}
+            className="w-9 h9 object-contain bg-white"
+          />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Hamza &nbsp;<span className="sm:block hidden">Khan-Benjamin</span>
+          </p>
         </Link>
+        <ul className={"list-none hidden sm:flex flex-row gap-10"}>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer `}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`} className=" ">
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className={"sm:hidden flex flex-1 justify-end items-center"}>
+          <img
+            src={toggle ? close : menu}
+            alt={menu}
+            className="w-[28px] h-[28px] cursor-pointer object-contain"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul
+              className={
+                "list-none flex justify-end items-start flex-col gap-4"
+              }
+            >
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } font-poppins text-[16px] font-medium cursor-pointer `}
+                  onClick={() => {
+                    setActive(link.title);
+                    setToggle(!toggle);
+                  }}
+                >
+                  <a href={`#${link.id}`} className=" ">
+                    {link.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
@@ -28,4 +88,4 @@ const Navbar = () => {
 
 export default Navbar;
 
-// End time 27.58
+// Get logo for navbar
