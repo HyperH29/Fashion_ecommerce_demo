c043ba8a262a98a141b7e494b310337cf20c9e90
Hamza <hkbenjamin1994@gmail.com>
2023-03-09 02:25:55 +0000

    Navbar is currently in the works.

--- Portfolio/Three_js_Protfolio/src/App.jsx
+++ Portfolio/Three_js_Protfolio/src/App.jsx
@@ -1,11 +1,36 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Tech,
+  Works,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+} from "./components";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello 3D World</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-patten bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- Portfolio/Three_js_Protfolio/src/components/Navbar.jsx
+++ Portfolio/Three_js_Protfolio/src/components/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles.js";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="flex items-center justify-between w-full max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt={logo} className="w-9 h9 object-contain" />
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
+
+// End time 27.58
